4288d1b186b770608239cdd8922be65420b33835
YeahitsDivyansh <divyansh972003@gmail.com>
2025-09-27 15:01:54 +0530

    feat: update server URL in UserContext and add .gitignore files for admin and song services

--- frontend/src/context/UserContext.tsx
+++ frontend/src/context/UserContext.tsx
@@ -8,7 +8,7 @@ import React, {
 } from "react";
 import toast, { Toaster } from "react-hot-toast";
 
-const server = "http://localhost:5000";
+const server = "https://spotify-user-service-pumj.onrender.com/";
 
 export interface User {
     _id: string;
