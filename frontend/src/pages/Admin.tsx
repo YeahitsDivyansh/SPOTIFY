50b5b237b753be891fc125f2bad70587b1efbbb2
YeahitsDivyansh <divyansh972003@gmail.com>
2025-09-27 15:16:06 +0530

    endpoints added

--- frontend/src/pages/Admin.tsx
+++ frontend/src/pages/Admin.tsx
@@ -6,7 +6,7 @@ import axios from "axios";
 import toast from "react-hot-toast";
 import { MdDelete } from "react-icons/md";
 
-const server = "http://localhost:7000";
+const server = "https://spotify-admin-service-6ei4.onrender.com/";
 
 const Admin = () => {
     const navigate = useNavigate();
